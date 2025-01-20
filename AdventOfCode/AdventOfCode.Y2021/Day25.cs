bdafc6376acb8ca3580be7ad1900cfe192350aa8
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2025-01-20 10:00:25 -0800

    2021 Day 25

---
+++ AdventOfCode/AdventOfCode.Y2021/Day25.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Map;
+
+namespace AdventOfCode.Y2021;
+
+public class Day25(string input) : IAdventDay
+{
+	private Map2D<char> Map { get; } = Map2D<char>.FromString(input);
+
+	private record Instruction(string Operaton, string Destination, string Modifier);
+
+	public string Part1()
+	{
+		var i = 0;
+		bool moved;
+		do
+		{
+			var movedEast = Move(Map, Direction.Right);
+			var movedSouth = Move(Map, Direction.Down);
+
+			moved = movedEast | movedSouth;
+			i++;
+		} while (moved);
+
+		return i.ToString();
+	}
+
+	private static bool Move(Map2D<char> current, Direction direction)
+	{
+		var herdChar = direction == Direction.Right ? '>' : 'v';
+		var herd = current.SearchAll(herdChar);
+
+		var changes = new List<(Position2D prev, Position2D next)>();
+
+		foreach (var item in herd)
+		{
+			var move = item.Move(direction);
+			if (current.OutOfBounds(move))
+				move = direction == Direction.Right ? new(0, move.Y) : new(move.X, 0);
+
+			if (current[move] == '.')
+				changes.Add((item, move));
+		}
+
+		foreach (var (item, next) in changes)
+		{
+			current[next] = herdChar;
+			current[item] = '.';
+		}
+
+		return changes.Count > 0;
+	}
+
+	//Day 25 does not have a part 2
+	public string Part2() => string.Empty;
+}
+
---
+++ AdventOfCode/AdventOfCode.Y2021Tests/Day25Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Y2021;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Y2021Tests;
+
+public class Day25Tests
+{
+
+	private readonly string input = @"v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>";
+
+	[Fact]
+	public void Part1SouldMatchExampleCount()
+	{
+		var day2 = new Day25(input);
+
+		var answer = day2.Part1();
+
+		answer.Should().Be("58");
+	}
+
+	[Fact]
+	public void Part2SouldMatchExampleCount()
+	{
+// Day 25 does not have a Part 2
+	}
+}
