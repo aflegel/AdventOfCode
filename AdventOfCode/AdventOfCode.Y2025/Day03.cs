2da0dd089818295115a2d94dc7efed7cb3ff8747
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2025-12-03 11:16:18 -0800

    2025 Day03

---
+++ AdventOfCode/AdventOfCode.Y2025/Day03.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode.Y2025;
+
+public class Day03(string input) : IAdventDay
+{
+	private string[] InputInstructions { get; } = [.. input.Split('\n')];
+
+	public string Part1() => InputInstructions.Sum(s => long.Parse(GetHighest(s, 2))).ToString();
+
+	private static string GetHighest(string battery, int depth)
+	{
+		if (depth == 0)
+			return "";
+		if (depth - 1 > battery.Length)
+			return "";
+
+		var max = battery[..^(depth - 1)].Select(s => s.ToInt()).Max().ToString();
+
+		return $"{max}{GetHighest(battery[(battery.IndexOf(max) + 1)..], depth - 1)}";
+	}
+
+	public string Part2() => InputInstructions.Sum(s => long.Parse(GetHighest(s, 12))).ToString();
+}
---
+++ AdventOfCode/AdventOfCode.Y2025Tests/Day03Tests.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Y2025;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Y2025Tests;
+
+public class Day03Tests
+{
+	private readonly string input = @"987654321111111
+811111111111119
+234234234234278
+818181911112111";
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day03(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("357");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day03(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("3121910778619");
+	}
+}
