23e01bad81244a8472ad271490b1ad1e354da399
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2025-12-03 22:45:20 -0800

    2025 Day 4

---
+++ AdventOfCode/AdventOfCode.Y2025/Day04.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.Core;
+using AdventOfCode.Map;
+
+namespace AdventOfCode.Y2025;
+
+public class Day04(string input) : IAdventDay
+{
+	private Map2D<char> InputMap { get; } = Map2D<char>.FromString(input);
+	private static readonly Direction[] adjacent = [Direction.Right, Direction.UpRight, Direction.Up, Direction.UpLeft, Direction.Left, Direction.DownLeft, Direction.Down, Direction.DownRight];
+
+	public string Part1() => GetRemovable(InputMap).Count().ToString();
+
+	private static IEnumerable<Position2D> GetRemovable(Map2D<char> map)
+	{
+		foreach (var pos in map.SearchAll('@'))
+		{
+			var adjacents = adjacent.Select(s => pos.Move(s)).Where(next => !map.OutOfBounds(next));
+			if (adjacents.Count(c => map[c] == '@') < 4)
+			{
+				yield return pos;
+			}
+		}
+	}
+
+	public string Part2()
+	{
+		var next = InputMap.Clone();
+		var count = 0;
+		int removes;
+		do
+		{
+			var removeList = GetRemovable(next).ToList();
+			foreach (var rem in removeList)
+			{
+				next[rem] = 'x';
+			}
+			removes = removeList.Count;
+			count += removes;
+		} while (removes > 0);
+
+		return count.ToString();
+	}
+}
---
+++ AdventOfCode/AdventOfCode.Y2025Tests/Day04Tests.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Y2025;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Y2025Tests;
+
+public class Day04Tests
+{
+	private readonly string input = @"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day04(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("13");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day04(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("43");
+	}
+}
