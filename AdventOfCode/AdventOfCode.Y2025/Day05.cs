b9f742b3100b72498d0940d6cf0f330d210db9a5
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2025-12-17 14:27:56 -0800

    2025 Day05

---
+++ AdventOfCode/AdventOfCode.Y2025/Day05.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode.Y2025;
+
+public class Day05(string input) : IAdventDay
+{
+	private (long start, long end)[] InputRanges { get; } = [.. input.Split("\n\n")[0].Split("\n").Select(s =>
+	{
+		var split = s.Split("-");
+		return (long.Parse(split[0]), long.Parse(split[1]));
+	})];
+	private long[] InputIds { get; } = [.. input.Split("\n\n")[1].Split("\n").Select(long.Parse)];
+
+	public string Part1() => InputIds.Count(w => InputRanges.Any(a => w >= a.start && w <= a.end)).ToString();
+
+	public string Part2() => Merge(InputRanges).Sum(s => s.end - s.start + 1).ToString();
+
+	private static (long start, long end)[] Merge((long start, long end)[] ranges)
+	{
+		var result = ranges.ToList();
+		var merged = new HashSet<(long start, long end)>();
+
+		var workDone = false;
+		do
+		{
+			workDone = false;
+			foreach (var (start, end) in result)
+			{
+				var overlap = merged.Where(s => (end >= s.start && end <= s.end) || (start >= s.start && start <= s.end)).ToList();
+
+				if (overlap.Count > 1)
+				{
+					workDone = true;
+					foreach (var item in overlap)
+						merged.Remove(item);
+
+					merged.Add((overlap.Concat([(start, end)]).Min(s => s.start), overlap.Max(s => s.end)));
+				}
+				else
+				{
+					merged.Add((start, end));
+				}
+			}
+			result = [.. merged];
+		}
+		while (workDone);
+
+		return [.. result];
+	}
+}
---
+++ AdventOfCode/AdventOfCode.Y2025Tests/Day05Tests.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode.Y2025;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Y2025Tests;
+
+public class Day05Tests
+{
+	private readonly string input = @"3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day05(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("3");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day05(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("14");
+	}
+}
