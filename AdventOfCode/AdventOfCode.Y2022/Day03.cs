8fe0f76d6fbbc90cca12d75dd655d76a674683e3
Alexander Boutin Flegel <alex@xanderlan.ca>
2026-01-05 09:08:58 -0800

    2022 Day03

---
+++ AdventOfCode/AdventOfCode.Y2022/Day03.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode.Y2022;
+
+public class Day03(string input) : IAdventDay
+{
+	private static readonly string score = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private string[] InputArray { get; } = [.. input.Split("\n")];
+	public string Part1() => InputArray.Sum(s =>
+	{
+		var len = s.Length / 2;
+		var r = s[..len];
+		var l = s[len..];
+
+		return score.IndexOf(r.Intersect(l).Single()) + 1;
+	}).ToString();
+
+	public string Part2() => InputArray.Chunk(3).Sum(s =>
+	{
+		var set = s.Select(s => s.Distinct()).ToList();
+
+		return score.IndexOf(s[0].Intersect(s[1]).Intersect(s[2]).Single()) + 1;
+	}).ToString();
+
+}
---
+++ AdventOfCode/AdventOfCode.Y2022Tests/Day03Tests.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode.Y2022;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode.Y2022Tests;
+
+public class Day03Tests
+{
+	private readonly string input = @"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+	[Fact]
+	public void Part1ShouldMatchExampleCount()
+	{
+		var day = new Day03(input);
+
+		var answer = day.Part1();
+
+		answer.Should().Be("157");
+	}
+
+	[Fact]
+	public void Part2ShouldMatchExampleCount()
+	{
+		var day = new Day03(input);
+
+		var answer = day.Part2();
+
+		answer.Should().Be("70");
+	}
+}
